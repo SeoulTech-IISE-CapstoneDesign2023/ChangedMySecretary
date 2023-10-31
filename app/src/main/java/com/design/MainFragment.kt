ae4c75e24d2975bb67f26c65d102953f0dd8c190
kimgdongju <unso99@gamil.com>
2023-10-31 14:50:27 +0900

    design: "fragment_main 디자인 추가"

--- app/src/main/java/com/design/MainFragment.kt
+++ app/src/main/java/com/design/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.design.databinding.FragmentMainBinding
+import com.design.util.FirebaseUtil
 
 class MainFragment:Fragment() {
 
@@ -24,6 +25,13 @@ class MainFragment:Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //이곳에서 ui작동
+        binding?.let {binding ->
+            FirebaseUtil.userDataBase.child("user_info").get()
+                .addOnSuccessListener {
+                    val value = it.getValue(User::class.java)
+                    binding.titleTextView.text = "${value?.nickname}님\n기분 좋은 하루 되세요"
+                }
+        }
     }
 
     override fun onDestroyView() {
