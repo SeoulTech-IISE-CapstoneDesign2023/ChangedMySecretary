107c2ffb9ca66405ea8882c00210e2c1a70f493e
Lee SeungJun <kidsj99@naver.com>
2023-12-03 00:42:37 +0900

    design : Intro 이미지 및 설명 수정

--- app/src/main/java/com/design/Profile/IntroActivity.kt
+++ app/src/main/java/com/design/Profile/IntroActivity.kt
@@ -37,17 +37,19 @@ class IntroActivity : AppCompatActivity() {
 
         // 슬라이드 이미지 및 설명 텍스트 배열 정의
         val slideImages = intArrayOf(
-            R.drawable.img_1,
-            R.drawable.img_4,
-            R.drawable.img_3,
-            R.drawable.img_2,
+            R.drawable.main,
+            R.drawable.calendar,
+            R.drawable.map2,
+            R.drawable.map,
+            R.drawable.create,
         )
 
-        val slideTexts = arrayOf("알람으로 늦지않게 도착하도록 도와드릴게요",
-            "날짜별로 여러 일정들을 관리해드릴게요",
-            "친구들과 공유해서 추억을 쌓아보아요",
-            "일정을 생성할 때 이동 경로를 알려드릴게요"
-
+        val slideTexts = arrayOf(
+            "실시간 업데이트 알람으로 늦지않게 도와드릴게요!",
+            "날짜별로 여러 일정들을 관리해드릴게요!",
+            "친구들과 공유해서 추억을 쌓아보아요!",
+            "친구들과 추억을 쌓은 장소를 확인해요!",
+            "추억 생성에 다양한 기능들을 사용해보아요!",
         )
 
         // ViewPager와 PagerAdapter 초기화
